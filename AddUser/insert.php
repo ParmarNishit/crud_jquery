f3ed74bfa5a33bad6f7e7d71f54bac723b629e1e
Nishit Parmar <nishit.parmar@cygnet-enterprise.com>
2024-12-26 14:09:50 +0530

    Password now store as hashed password

--- AddUser/insert.php
+++ AddUser/insert.php
@@ -5,7 +5,9 @@ include '/xampp/htdocs/crud_jquery/DBConnection/connection.php';
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$query = "insert into users (username, password) VALUES ('{$username}', '{$password}') ";
+$hasedpassword = password_hash($password, PASSWORD_DEFAULT);
+
+$query = "insert into users (username, password) VALUES ('{$username}', '{$hasedpassword}') ";
 $result = mysqli_query($connection, $query);
 
 if ($result) {
